6a5aa519e5f835e8d58fe85ef59f709a2de4e99d
juan8824 <juan882804@gmail.com>
2023-04-11 01:52:10 -0300

    first_commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,43 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const users = await User.findAll();
+    return res.json(users)
+});
+
+const create = catchError(async(req, res) => {
+    const {first_name, last_name, email, password, birthday} = req.body;
+    const users = await User.create({first_name, last_name, email, password, birthday})
+    return res.status(201).json(users);
+});
+
+const getUser = catchError(async(req, res) => {
+    const {id} = req.params;
+    const user = await User.findByPk(id);
+    return res.json(user);
+})
+
+const remove = catchError(async(req, res) => {
+    const {id} = req.params;
+    await User.destroy({where: {id}});
+    return res.sendStatus(204);
+})
+
+const update = catchError(async(req, res) => {
+    const {id} = req.params;
+    const {first_name, last_name, email, password, birthday} = req.body;
+    const user = await User.update(
+        {first_name, last_name, email, password, birthday},
+        {where: {id}, returning: true}
+    );
+    return res.json(user[1][0]);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getUser,
+    remove,
+    update
+}
